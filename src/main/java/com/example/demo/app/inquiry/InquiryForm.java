bfd0d36749f2991f727dcd74d1e5bf8b01ecc151
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-02-09 23:30:29 +0900

    make a form

---
+++ src/main/java/com/example/demo/app/inquiry/InquiryForm.java
@@ -0,0 +1,40 @@
+package com.example.demo.app.inquiry;
+
+public class InquiryForm {
+	
+	private String name;
+	private String email;
+	private String contents;
+	
+	
+	
+	
+	public InquiryForm() {
+		
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+
+}
