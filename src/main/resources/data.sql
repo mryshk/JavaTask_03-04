847f8d9e47b69d11c5606c7153d67f129990078c
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-02-09 23:30:19 +0900

    Connect db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO inquiry(name,email,contents, created)
+VALUES('Ethan','sample@sample.com','Hello','2019-11-12 08:34:23');
+INSERT INTO inquiry(name,email,contents, created)
+VALUES('Emma','sample2@sample.com','GoodEvening','2021-10-22 04:35:53');
+INSERT INTO inquiry(name,email,contents, created)
+VALUES('William','sample3@sample.com','GoodMorning','2022-01-12 03:36:13');
