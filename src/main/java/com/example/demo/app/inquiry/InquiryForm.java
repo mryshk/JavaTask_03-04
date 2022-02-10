4dfd34b7d7421bd8de40f48679725848dc4fda29
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-02-10 21:46:21 +0900

    modify

--- src/main/java/com/example/demo/app/inquiry/InquiryForm.java
+++ src/main/java/com/example/demo/app/inquiry/InquiryForm.java
@@ -1,9 +1,21 @@
 package com.example.demo.app.inquiry;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
 public class InquiryForm {
 	
+	
+	@Size(min=1, max=20, message="Please input 20 characters or less")
 	private String name;
+	
+	@NotNull
+	@Email(message="Invalid E-mail Format")
 	private String email;
+	
+	@NotNull
 	private String contents;
 	
 	
