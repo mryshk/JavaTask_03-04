b1a3a796284391978a84203a6096e1fd7745d8a5
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-02-13 23:34:18 +0900

    Add survey

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,11 @@ INSERT INTO inquiry(name,email,contents, created)
 VALUES('Emma','sample2@sample.com','GoodEvening','2021-10-22 04:35:53');
 INSERT INTO inquiry(name,email,contents, created)
 VALUES('William','sample3@sample.com','GoodMorning','2022-01-12 03:36:13');
+
+
+INSERT INTO survey(age, satisfaction, comment, created)
+VALUES(23, 5, 'Good!', '2019-01-03 11:02:35');
+INSERT INTO survey(age, satisfaction, comment, created)
+VALUES(47, 4, 'Excellent', '2019-02-18 22:35:54');
+INSERT INTO survey(age, satisfaction, comment, created)
+VALUES(47, 4, 'Excellent', '2019-02-18 22:35:54');
